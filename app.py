71547c1dcb33e62a93fbc09f3a3ef0ad85e2eda9
Теняков Антон Борисович <a.tenyakov@t1-group.ru>
2021-01-10 00:06:41 +0300

    added error

--- app.py
+++ app.py
@@ -9,13 +9,11 @@ api = Api(app)
 def check_usdot(usdot:int):
     # Check USDOT in site https://safer.fmcsa.dot.gov/
 
-    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.binary_location = GOOGLE_CHROME_PATH
-    browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
 
     try:
         browser.get('https://safer.fmcsa.dot.gov/CompanySnapshot.aspx')
